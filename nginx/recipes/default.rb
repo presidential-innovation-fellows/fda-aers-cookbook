50c079c34478c1c478f137869b1cf2764e786398
seanherron <seanherron@gmail.com>
2013-09-05 16:34:38 -0400

    added nginx config

---
+++ nginx/recipes/default.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+#
+
+package "nginx" do
+  action [:install]
+end
+
+template "/etc/nginx/sites-available/application" do
+  source "application.erb"
+  variables({
+    :app_name => node[:deploy][:application],
+    :serve_port => node[:nginx][:serve_port],
+    :root_dir => node[:deploy][:deploy_to],
+    :log_dir => node[:nginx][:log_dir],
+    })
+end
+
+link "/etc/nginx/sites-enabled/application" do
+  to "/etc/nginx/sites-available/application"
+end
+
+service "nginx" do
+  action[:enable,:start]
+end
