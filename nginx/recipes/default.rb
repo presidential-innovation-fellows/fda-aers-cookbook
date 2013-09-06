399374a2b1b732467aae394f5e879c84c61d3a7a
seanherron <seanherron@gmail.com>
2013-09-06 11:39:55 -0400

    no {}

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -11,10 +11,10 @@ end
 template "/etc/nginx/sites-available/application" do
   source "application.erb"
   variables({
-    :app_name => #{node[:deploy][:application]},
-    :serve_port => #{node[:nginx][:serve_port]},
-    :root_dir => #{node[:deploy][:deploy_to]},
-    :log_dir => #{node[:nginx][:log_dir]},
+    :app_name => node[:deploy][:application][0],
+    :serve_port => node[:nginx][:serve_port][0],
+    :root_dir => node[:deploy][:deploy_to][0],
+    :log_dir => node[:nginx][:log_dir][0],
     })
 end
 
