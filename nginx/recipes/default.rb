fc6478eb0e241d94425e3d0fa6da1a5636ee3d04
seanherron <seanherron@gmail.com>
2013-09-06 11:48:26 -0400

    trying to clean up

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -3,7 +3,6 @@
 # Recipe:: default
 #
 #
-
 package "nginx" do
   action [:install]
 end
@@ -11,10 +10,10 @@ end
 template "/etc/nginx/sites-available/application" do
   source "application.erb"
   variables({
-    :app_name => node[:deploy][:application][0],
-    :serve_port => node[:nginx][:serve_port][0],
-    :root_dir => node[:deploy][:deploy_to][0],
-    :log_dir => node[:nginx][:log_dir][0],
+    :app_name => node[:deploy][:application],
+    :serve_port => node[:nginx][:serve_port],
+    :root_dir => node[:deploy][:deploy_to],
+    :log_dir => node[:nginx][:log_dir],
     })
 end
 
