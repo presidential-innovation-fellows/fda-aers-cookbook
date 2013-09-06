99ac29fa7eb24b27e958acb2dfca6717b36af9ad
seanherron <seanherron@gmail.com>
2013-09-06 11:21:57 -0400

    trying more with template variables

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -10,6 +10,12 @@ end
 
 template "/etc/nginx/sites-available/application" do
   source "application.erb"
+  variables({
+    :app_name => #{node[:deploy][:application]},
+    :serve_port => #{node[:nginx][:serve_port]},
+    :root_dir => #{node[:deploy][:deploy_to]},
+    :log_dir => #{node[:nginx][:log_dir]},
+    })
 end
 
 link "/etc/nginx/sites-enabled/application" do
